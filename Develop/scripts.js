70ef52b5562c9a1b6ead3c2a87d73c0fa00773df
mo-aden <mohamedaden17@outlook.com>
2022-11-20 19:56:29 -0700

    updated the functionally of searched cities click function

--- Develop/scripts.js
+++ Develop/scripts.js
@@ -1,33 +1,29 @@
-const searchBtn = document.querySelector("#searchBtn");
-const userInput = document.querySelector(".inputField");
+const searchBtnEl = document.querySelector("#searchBtn");
+const userInputEl = document.querySelector(".inputField");
 const searchTextEl = document.querySelector(".searchText");
+const searchedCitiesEl = document.querySelector(".searchedCities");
+const cityNameEl = document.querySelector("#city-name");
 
+//Get userInputs from local storage
+const searchedCities = JSON.parse(localStorage.getItem("cities")) || [];
 const apiKey = "06e20dcd9d8864feaf509bbb85437340";
-let cities = [];
 
-//event fun for submit
-searchBtn.addEventListener("click", function (e) {
-  e.preventDefault();
+//Get the lattitude and longitude of cities
+function fetchCurrentWeather(lat, lon) {
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
 
-  const inputValue = userInput.value;
-  cities.push(inputValue);
+  fetch(apiURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //Display data onto the page
+      // console.log(data);
+      displayWeatherInfo(data);
+    });
+}
 
-  localStorage.setItem("userInputs", JSON.stringify(cities));
-
-  fetchCityCoordinates(inputValue);
-  //   console.log(userValue);
-});
-
-//Display user input values
-const inputValues = JSON.parse(localStorage.getItem("userInputs")) || [];
-
-console.log(inputValues);
-
-searchTextEl.innerHTML = inputValues
-  .map((item) => {
-    return `<li> ${item} <li>`;
-  })
-  .join("");
+//Using the user input get the weather data from the api
 
 function fetchCityCoordinates(city) {
   const apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${1}&appid=${apiKey}`;
@@ -39,19 +35,65 @@ function fetchCityCoordinates(city) {
     .then(function (data) {
       //   console.log(data[0].lat);
       //   console.log(data[0].lon);
-
       fetchCurrentWeather(data[0].lat, data[0].lon);
     });
 }
 
-function fetchCurrentWeather(lat, lon) {
-  const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+//Handle form submit
+function handleFormSubmit(e) {
+  e.preventDefault();
 
-  fetch(apiURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data.main);
-    });
+  const userInput = userInputEl.value;
+  searchedCities.push(userInput); //pushes to the arr
+
+  //store userinput in the local storage
+  localStorage.setItem("cities", JSON.stringify(searchedCities));
+  // localStorage.setItem("userInputs", JSON.stringify(cities));
+
+  fetchCityCoordinates(userInput);
+
+  //   console.log(userValue);
+}
+
+//event fun for submit
+searchBtnEl.addEventListener("click", handleFormSubmit);
+
+//Display searched Cities only last 5
+function DisplaySearchedCities() {
+  const recentCities = searchedCities.slice(-5, searchedCities.length);
+  // console.log(recentCities);
+
+  //Looping backwords to show the user recent searches
+  for (let i = recentCities.length - 1; i >= 0; i--) {
+    const city = recentCities[i];
+    // console.log(city);
+
+    //Create HTML element for each city
+    const searchedCityBtnEl = document.createElement("button");
+    searchedCityBtnEl.setAttribute("class", "city-searched");
+    searchedCityBtnEl.textContent = city;
+
+    //append it to the page
+    searchedCitiesEl.append(searchedCityBtnEl);
+  }
+}
+
+DisplaySearchedCities();
+
+//Event for searched City Button click
+function handleSearchedcityClick(e) {
+  e.preventDefault();
+  console.log(e.target.textContent);
+
+  const cityInput = e.target.textContent;
+  //call the api for new city input from the button
+  fetchCityCoordinates(cityInput);
+}
+
+searchedCitiesEl.addEventListener("click", handleSearchedcityClick);
+
+function displayWeatherInfo(data) {
+  console.log(data);
+
+  cityNameEl.textContent = data.name;
 }
