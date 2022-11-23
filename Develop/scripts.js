27c671f1a27d5560edca6ab4ea944029c212e346
mo-aden <mohamedaden17@outlook.com>
2022-11-22 18:32:08 -0700

    added alert function for non existing cities

--- Develop/scripts.js
+++ Develop/scripts.js
@@ -2,38 +2,15 @@ const searchBtnEl = document.querySelector("#searchBtn");
 const userInputEl = document.querySelector(".inputField");
 const searchTextEl = document.querySelector(".searchText");
 const searchedCitiesEl = document.querySelector(".searchedCities");
-const cityNameEl = document.querySelector("#city-name");
-const dateEl = document.querySelector("#date");
-const iconEl = document.querySelector("#icon");
-const tempEl = document.querySelector("#temp");
-const supscriptEl = document.querySelector("#temp sup");
-const descriptionEL = document.querySelector("#description");
-const windEl = document.querySelector("#wind");
-const humidityEl = document.querySelector("#humidity");
 const clearBtnEl = document.querySelector("#clearBtn");
 const randomCardsEl = document.querySelector(".random-cards");
+const searchHeadingEl = document.querySelector(".search-heading");
 
 //Get userInputs from local storage
 const searchedCities = JSON.parse(localStorage.getItem("cities")) || [];
 const apiKey = "06e20dcd9d8864feaf509bbb85437340";
 
-//Get the lattitude and longitude of cities
-function fetchCurrentWeather(lat, lon) {
-  const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-
-  fetch(apiURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      //Display data onto the page
-      // console.log(data);
-      // displayWeatherInfo(data);
-    });
-}
-
 //Using the user input get the weather data from the api
-
 function fetchCityCoordinates(city) {
   const apiURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${1}&appid=${apiKey}`;
 
@@ -42,20 +19,26 @@ function fetchCityCoordinates(city) {
       return response.json();
     })
     .then(function (data) {
-      //   console.log(data[0].lat);
-      //   console.log(data[0].lon);
-      fetchCurrentWeather(data[0].lat, data[0].lon);
+      //Call forcast fn and set its property for lat & lon
       fetchForcast(data[0].lat, data[0].lon);
 
       searchedCities.push(city); //pushes to the arr
 
       //store userinput in the local storage
       localStorage.setItem("cities", JSON.stringify(searchedCities));
-      // localStorage.setItem("userInputs", JSON.stringify(cities));
+
+      //call this fn to show the new entries
       DisplaySearchedCities();
     })
     .catch((err) => {
-      console.log(`Enter correct city ...`);
+      //add class and change txt content
+      searchHeadingEl.classList.add("alert");
+      searchHeadingEl.textContent = `City Doesn't exist 🟥!`;
+
+      setTimeout(() => {
+        //Refresh the page
+        window.location.reload();
+      }, 1000);
     });
 }
 
@@ -68,12 +51,7 @@ function fetchForcast(lat, lon) {
       return response.json();
     })
     .then(function (data) {
-      //Display data onto the page
-      // console.log(data);
-      // console.log(data);
-      // displayWeatherInfo(data);
-      // displayToday(data);
-      // displayWeatherInfo(data);
+      //using this data display random cards to the user
       displayRandomCards(data);
     });
 }
@@ -83,32 +61,26 @@ function handleFormSubmit(e) {
   e.preventDefault();
 
   const userInput = userInputEl.value;
-  // searchedCities.push(userInput); //pushes to the arr
 
-  // //store userinput in the local storage
-  // localStorage.setItem("cities", JSON.stringify(searchedCities));
-  // // localStorage.setItem("userInputs", JSON.stringify(cities));
-  // DisplaySearchedCities();
-
+  //remove exisitng content on cards
+  randomCardsEl.textContent = "";
+  //call the fn to use the user input
   fetchCityCoordinates(userInput);
-
-  //   console.log(userValue);
 }
 
-//event fun for submit
+//event click fun for submit
 searchBtnEl.addEventListener("click", handleFormSubmit);
 
 //Display searched Cities only last 5
 function DisplaySearchedCities() {
+  //Only show the user the last 5 cities
   const recentCities = searchedCities.slice(-5, searchedCities.length);
-  // console.log(recentCities);
 
-  //Clear
+  //Clear exisitng txt
   searchedCitiesEl.textContent = "";
-  //Looping backwords to show the user recent searches
+  //Looping backwords to show the user most recent searches
   for (let i = recentCities.length - 1; i >= 0; i--) {
     const city = recentCities[i];
-    // console.log(city);
 
     //Create HTML element for each city
     const searchedCityBtnEl = document.createElement("button");
@@ -125,7 +97,10 @@ function DisplaySearchedCities() {
 //Event for searched City Button click
 function handleSearchedcityClick(e) {
   e.preventDefault();
-  console.log(e.target.textContent);
+  // console.log(e.target.textContent);
+
+  //Remove exisitng content on cards
+  randomCardsEl.textContent = "";
 
   const cityInput = e.target.textContent;
   //call the api for new city input from the button
@@ -147,80 +122,39 @@ clearBtnEl.addEventListener("click", function (e) {
   window.location.reload();
 });
 
-//Randonly generated cards
-function displayToday(data) {
-  const cityName = data.city.name;
-  // console.log(cityName);
-  const country = data.city.country;
-  // console.log(country);
-
-  let dt = new Date(data.list[0].dt_txt);
-  const today = dt.toLocaleDateString("en-us", { weekday: "long", year: "numeric", month: "numeric", day: "numeric" });
-  // console.log(today);
-
-  // const icon = data.list.weather;
-
-  const iconUrl = `https://openweathermap.org/img/wn/${data.list[0].weather[0]["icon"]}.png`;
-
-  const temp = data.list[0].main.temp;
-  // console.log(temp);
-
-  const maxTemp = data.list[0].main.temp_max;
-  const minTemp = data.list[0].main.temp_min;
-
-  // console.log(maxTemp, minTemp);
-
-  const humidity = data.list[0].main.humidity;
-  // console.log(humidity);
-
-  const wind = data.list[0].wind.speed;
-
-  cityNameEl.textContent = `${cityName} - ${country}`;
-  dateEl.textContent = today;
-  iconEl.src = iconUrl;
-  tempEl.textContent = `${temp} ${"\u00B0F"}`;
-  descriptionEL.textContent = `Highs ${maxTemp} & Lows ${minTemp}`;
-  windEl.textContent = `Wind Speed = ${wind} MPH`;
-  humidityEl.textContent = `Humidity = ${humidity} %`;
-}
-
 function displayRandomCards(data) {
-  // console.log(data);
-
   //Data not changing
   const cityName = data.city.name;
-  // console.log(cityName);
   const country = data.city.country;
-  // console.log(country);
 
+  //changing data
   const dataArr = data.list;
-  // console.log(dataArr);
 
   for (let i = 1; i < dataArr.length; i += 8) {
-    const el = dataArr[i];
-    console.log(el);
+    const item = dataArr[i];
 
-    let dt = new Date(data.list[i].dt_txt);
-    const today = dt.toLocaleDateString("en-us", { weekday: "long", year: "numeric", month: "numeric", day: "numeric" });
-    // console.log(today);
+    const dateNow = new Date(item.dt_txt).toLocaleDateString("en-us", { weekday: "long", year: "numeric", month: "numeric", day: "numeric" });
 
-    const iconUrl = `https://openweathermap.org/img/wn/${data.list[i].weather[0]["icon"]}.png`;
+    const iconUrl = `https://openweathermap.org/img/wn/${item.weather[0]["icon"]}.png`;
 
+    //Create a div container
     const div = document.createElement("div");
     div.setAttribute("class", "card");
 
+    //add inner HTML to the div
     div.innerHTML = `   
           <div class="card-body">
             <p id="city-name">${cityName} - ${country}</p>
-            <p id="date">${today}</p>
+            <p id="date">${dateNow}</p>
             <img id="icon" src=${iconUrl} alt="weather Icon" />
-            <p id="temp">${data.list[i].main.temp} ${"\u00B0F"}</p>
-            <p id="description">Highs ${data.list[i].main.temp_max} & Lows ${data.list[i].main.temp_min}</p>
-            <p id="wind">Wind: ${data.list[i].wind.speed}</p>
-            <p id="humidity">Humidity: ${data.list[i].main.humidity} %</p>
+            <p id="temp">${item.main.temp} ${"\u00B0F"}</p>
+            <p id="description">Highs ${item.main.temp_max} & Lows ${item.main.temp_min}</p>
+            <p id="wind">Wind: ${item.wind.speed} MPH</p>
+            <p id="humidity">Humidity: ${item.main.humidity} %</p>
           </div>
         `;
 
+    //append the div to the parent div
     randomCardsEl.append(div);
   }
 }
